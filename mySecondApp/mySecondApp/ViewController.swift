561861baf85825588389644a019f06a463141dac
Mauricio Cuadros <a01236054@tec.mx>
2022-10-05 08:52:57 -0500

    pre Calculator

--- mySecondApp/mySecondApp/ViewController.swift
+++ mySecondApp/mySecondApp/ViewController.swift
@@ -11,7 +11,104 @@ class ViewController: UIViewController {
     @IBOutlet weak var tituloLabel: UILabel!
     @IBOutlet weak var usuarioTextField: UITextField!
     @IBOutlet weak var contrasenaTextField: UITextField!
+    @IBOutlet weak var numeroSuperior: UITextField!
+    @IBOutlet weak var numeroInferior: UITextField!
     
+    @IBAction func suma(_ sender: Any) {
+        let numUno = Double(numeroSuperior.text!)
+        let numDos = Double(numeroInferior.text ?? "0")
+        var mensaje = "Favor de completar ambos campos."
+        
+        if (numUno != nil && numDos != nil) {
+            let resultado = (numDos ?? 0) + (numUno ?? 0)
+            mensaje = String(resultado)
+        }
+        
+        
+        let alert = UIAlertController(
+                    title: "¡Suma!",
+                    message: mensaje,
+                    preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(
+            title: "Cerrar",
+            style: UIAlertAction.Style.cancel))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func resta(_ sender: Any) {
+        let numUno = Double(numeroSuperior.text!)
+        let numDos = Double(numeroInferior.text ?? "0")
+        var mensaje = "Favor de completar ambos campos."
+        
+        if (numUno != nil && numDos != nil) {
+            let resultado = (numUno ?? 0) - (numDos ?? 0)
+            mensaje = String(resultado)
+        }
+        
+        
+        let alert = UIAlertController(
+                    title: "¡Resta!",
+                    message: mensaje,
+                    preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(
+            title: "Cerrar",
+            style: UIAlertAction.Style.cancel))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func multiplicacion(_ sender: Any) {
+        let numUno = Double(numeroSuperior.text!)
+        let numDos = Double(numeroInferior.text ?? "0")
+        var mensaje = "Favor de completar ambos campos."
+        
+        if (numUno != nil && numDos != nil) {
+            let resultado = (numDos ?? 0) * (numUno ?? 0)
+            mensaje = String(resultado)
+        }
+        
+        
+        let alert = UIAlertController(
+                    title: "¡Multiplicación!",
+                    message: mensaje,
+                    preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(
+            title: "Cerrar",
+            style: UIAlertAction.Style.cancel))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func division(_ sender: Any) {
+        //let dividendo = numeroInferior.text
+        //let divisor = numeroSuperior.text
+        
+        let numDos = Double(numeroSuperior.text!)
+        let numUno = Double(numeroInferior.text ?? "0")
+        var mensaje = "Favor de completar ambos campos."
+        
+        if (numUno != nil && numDos != nil) {
+            let resultado = (numUno ?? 0) / (numDos ?? 0)
+            mensaje = String(resultado)
+        }
+        
+        
+        let alert = UIAlertController(
+                    title: "¡División!",
+                    message: mensaje,
+                    preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(
+            title: "Cerrar",
+            style: UIAlertAction.Style.cancel))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
     
     @IBAction func botonUnoClick(_ sender: Any) {
         let alert = UIAlertController(
